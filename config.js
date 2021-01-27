6b70630ec12c2c10244181e5ad9bf3c3d38690f8
timo04172003 <75484404+timo04172003@users.noreply.github.com>
2021-01-27 09:30:17 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    groupId: 0,
-    maximumRank: 0,
+    groupId: 6141441,
+    maximumRank: 233,
     prefix: 'q!',
     logChannelId: '',
     shoutChannelId: '',
@@ -12,7 +12,7 @@ module.exports = {
         success: '#43d177'
     },
     verificationChecks: false,
-    firedRank: 1,
+    firedRank: 2,
     xpRankup: {
         enabled: false,
         roles: [
@@ -20,6 +20,56 @@ module.exports = {
             //     rank: 2,
             //     xpNeeded: 10
             // }
+            Auxilia: {
+                rank: 2,
+                xpNeeded: 0
+                hpNeeded: 0
+            }
+            Tiro: {
+                rank: 150,
+                xpNeeded: 15
+                hpNeeded: 0
+            }
+            Milites: {
+                rank: 200,
+                xpNeeded: 30
+                hpNeeded: 0
+            }
+            Immunes: {
+                rank: 205,
+                xpNeeded: 60
+                hpNeeded: 0
+            }
+            Lictoris: {
+                rank: 206,
+                xpNeeded: 120
+                hpNeeded: 0
+            }
+            Sesquiplicarius: {
+                rank: 214,
+                xpNeeded: 180
+                hpNeeded: 5
+            }
+            Duplicarius: {
+                rank: 215,
+                xpNeeded: 240
+                hpNeeded: 25
+            }
+            Triplicarius: {
+                rank: 219,
+                xpNeeded: 300
+                hpNeeded: 25
+            }
+            Optio: {
+                rank: 230,
+                xpNeeded: 300
+                hpNeeded: 50
+            }
+            Centurion: {
+                rank: 233,
+                xpNeeded: 300
+                hpNeeded: 100
+            }
         ]
     },
     memberCount: {
